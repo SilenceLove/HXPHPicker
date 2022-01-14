54859df07dc7e3ff69b51b48b3312c4ea3f0582f
hongxin <294005139@qq.com>
2022-01-14 14:47:46 +0800

    Update Example

--- Example/Classes/Controller/PickerResultViewController.swift
+++ Example/Classes/Controller/PickerResultViewController.swift
@@ -500,7 +500,7 @@ class PickerResultViewController: UIViewController,
                 }
                 return
             }
-            func saveImage(_ image: UIImage) {
+            func saveImage(_ image: Any) {
                 AssetManager.saveSystemAlbum(forImage: image) { phAsset in
                     if phAsset != nil {
                         photoBrowser.view.hx.showSuccess(text: "保存成功", delayHide: 1.5, animated: true)
@@ -535,9 +535,8 @@ class PickerResultViewController: UIViewController,
                                 }
                             })
                         }else {
+                            saveImage(response.url)
                             photoBrowser.view.hx.hide(animated: true)
-                            let image = UIImage(contentsOfFile: response.url.path)!
-                            saveImage(image)
                         }
                     }else {
                         if response.urlType == .network {
@@ -851,57 +850,49 @@ extension PickerResultViewController: PhotoPickerControllerDelegate {
         let lyricUrl1 = Bundle.main.url(forResource: "天外来物", withExtension: nil)!
         let lrc1 = try! String(contentsOfFile: lyricUrl1.path) // swiftlint:disable:this force_try
         let music1 = VideoEditorMusicInfo.init(audioURL: audioUrl1,
-                                               lrc: lrc1,
-                                               urlType: .network)
+                                               lrc: lrc1)
         musics.append(music1)
         let audioUrl2 = Bundle.main.url(forResource: "嘉宾", withExtension: "mp3")!
         let lyricUrl2 = Bundle.main.url(forResource: "嘉宾", withExtension: nil)!
         let lrc2 = try! String(contentsOfFile: lyricUrl2.path) // swiftlint:disable:this force_try
         let music2 = VideoEditorMusicInfo.init(audioURL: audioUrl2,
-                                               lrc: lrc2,
-                                               urlType: .network)
+                                               lrc: lrc2)
         musics.append(music2)
         let audioUrl3 = Bundle.main.url(forResource: "少女的祈祷", withExtension: "mp3")!
         let lyricUrl3 = Bundle.main.url(forResource: "少女的祈祷", withExtension: nil)!
         let lrc3 = try! String(contentsOfFile: lyricUrl3.path) // swiftlint:disable:this force_try
         let music3 = VideoEditorMusicInfo.init(audioURL: audioUrl3,
-                                               lrc: lrc3,
-                                               urlType: .network)
+                                               lrc: lrc3)
         musics.append(music3)
         let audioUrl4 = Bundle.main.url(forResource: "野孩子", withExtension: "mp3")!
         let lyricUrl4 = Bundle.main.url(forResource: "野孩子", withExtension: nil)!
         let lrc4 = try! String(contentsOfFile: lyricUrl4.path) // swiftlint:disable:this force_try
         let music4 = VideoEditorMusicInfo.init(audioURL: audioUrl4,
-                                               lrc: lrc4,
-                                               urlType: .network)
+                                               lrc: lrc4)
         musics.append(music4)
         let audioUrl5 = Bundle.main.url(forResource: "无赖", withExtension: "mp3")!
         let lyricUrl5 = Bundle.main.url(forResource: "无赖", withExtension: nil)!
         let lrc5 = try! String(contentsOfFile: lyricUrl5.path) // swiftlint:disable:this force_try
         let music5 = VideoEditorMusicInfo.init(audioURL: audioUrl5,
-                                               lrc: lrc5,
-                                               urlType: .network)
+                                               lrc: lrc5)
         musics.append(music5)
         let audioUrl6 = Bundle.main.url(forResource: "时光正好", withExtension: "mp3")!
         let lyricUrl6 = Bundle.main.url(forResource: "时光正好", withExtension: nil)!
         let lrc6 = try! String(contentsOfFile: lyricUrl6.path) // swiftlint:disable:this force_try
         let music6 = VideoEditorMusicInfo.init(audioURL: audioUrl6,
-                                               lrc: lrc6,
-                                               urlType: .network)
+                                               lrc: lrc6)
         musics.append(music6)
         let audioUrl7 = Bundle.main.url(forResource: "世间美好与你环环相扣", withExtension: "mp3")!
         let lyricUrl7 = Bundle.main.url(forResource: "世间美好与你环环相扣", withExtension: nil)!
         let lrc7 = try! String(contentsOfFile: lyricUrl7.path) // swiftlint:disable:this force_try
         let music7 = VideoEditorMusicInfo.init(audioURL: audioUrl7,
-                                               lrc: lrc7,
-                                               urlType: .network)
+                                               lrc: lrc7)
         musics.append(music7)
         let audioUrl8 = Bundle.main.url(forResource: "爱你", withExtension: "mp3")!
         let lyricUrl8 = Bundle.main.url(forResource: "爱你", withExtension: nil)!
         let lrc8 = try! String(contentsOfFile: lyricUrl8.path) // swiftlint:disable:this force_try
         let music8 = VideoEditorMusicInfo.init(audioURL: audioUrl8,
-                                               lrc: lrc8,
-                                               urlType: .network)
+                                               lrc: lrc8)
         musics.append(music8)
         return musics
     }
